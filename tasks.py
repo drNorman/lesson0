1daf148a4a6843158f949451e8907ec3f4d127ed
drNorman <71461772+drNorman@users.noreply.github.com>
2024-08-31 14:45:06 +0300

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,18 @@
+#1st program
+print(9 ** 0.5 * 5)
+
+# 2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+
+# 4st program
+s = '123.456'
+n = float(s)
+n *= 10
+n = int(n)
+n = str(n)
+print(n[-1])
